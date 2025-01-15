c8b396e5053625d93cd8cd1643c1f66524b38e1d
Birajbgt <virajbogati11@gmail.com>
2025-01-15 12:29:15 +0545

    model

---
+++ model/userModel.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: Number,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordHistory: [
+        {
+            type: String,
+        },
+    ],
+    resetPasswordOTP: {
+        type: Number,
+        default: null
+    },
+    resetPasswordExpires: {
+        type: Date,
+        default: null
+    },
+    passwordChangedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    loginAttempts: {
+        type: Number,
+        default: 0,
+    },
+    lockUntil: {
+        type: Date,
+    },
+    isEmailVerified: { type: Boolean, default: false },
+    emailVerificationToken: String,
+    emailVerificationTokenExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+userSchema.virtual("isLocked").get(function () {
+    return !!(this.lockUntil && this.lockUntil > Date.now());
+});
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User;
