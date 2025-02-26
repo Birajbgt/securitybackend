26e4f654d6795f1581236566754a2706a0c9b4ef
Birajbgt <virajbogati11@gmail.com>
2025-02-26 21:12:42 +0545

    done

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,5 +1,5 @@
 const userModel = require('../model/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const sendOtp = require('../service/sendOtp');
 const sendEmail = require('./sendEmail');
